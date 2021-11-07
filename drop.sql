e08842833a8fed2dcac8362fd1097093598a6959
Sundus AL KEBSI <sundus.alkebsi@gmail.com>
2021-11-07 16:53:58 +0100

    add file drop.sql, that delets all tables created in creation.sql

---
+++ drop.sql
@@ -0,0 +1,24 @@
+-----------------------------------
+drop table Dispose;
+drop table Liste_d_achat;
+drop table Recette_contient_Ingredient;
+drop table Interdit;
+-------------------------------------
+drop table Ingrediant;
+-------------------------------------
+drop table Menu;
+drop table Qualite;
+drop table Img_vid;
+drop table Etape;
+-------------------------------------
+drop table Recette;
+-------------------------------------
+
+------------------------------------
+drop table Categorie;
+drop table Regime;
+drop table Utilisateur;
+------------------------------------
+drop table ArchiveMenu;
+drop table ArchiveDispose;
+drop table ArchiveListe;
